45cc4a72db542fbf37235b480895d1687a4efeab
Terry-404 <gamecorner71@gmail.com>
2022-04-16 02:03:08 +0200

    Feature: Date to UNIX Epoch api endpoint ([]/api/*-*-*)

--- server.js
+++ server.js
@@ -25,6 +25,29 @@ app.get("/api", (req,res)=>{
   res.json({"unix":unix, "utc":time.toUTCString()})
 })
 
+
+app.get("/api/:time", (req,res)=>{
+
+  //check if it's a valid date using Regex & Check if days are 31 or lower
+  if(req.params.time.match(/[0-9][0-9][0-9][0-9]-1[0-2]-[1-3][0-9]/) || req.params.time.match(/[0-9][0-9][0-9][0-9]-[1-9]-[1-3][0-9]/) && Number.parseInt(req.params.time.split("-")[2]) <= 31){
+    let dateSplitted = req.params.time.split("-");
+    let year = dateSplitted[0]
+    let month = dateSplitted[1]
+    let day = dateSplitted[2]
+
+    //intilize time in UTC timezone
+    let dateNow = new Date(`${year}-${month}-${day} 00:00:00 UTC`);
+    //conver it to UNIX
+    let unix = Date.parse(dateNow)
+
+
+    res.json({"unix":unix,"utc":dateNow.toUTCString()})
+  }
+  else{
+    res.json({"error":"Invalid Date"});
+  }
+})
+
 // listen for requests :)
 const listener = app.listen(3000 || process.env.PORT,()=>{
   console.log('Your app is listening on port ' + listener.address().port);
